4245bcabb60cb789e761a91e9a5f671d4af1d26d
Vanessa de La Cuetara <vdelacuetara@gmail.com>
2018-07-09 17:48:10 -0400

    finish styling of bloc jams

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
+import '../css/playerBar.css';
+
 
 class PlayerBar extends Component {
     render() {
         return (
             <section className="player-bar">
-                <section id="buttons">
-                    <button id="previous" onClick={this.props.handlePrevClick}>
-                        <span className="ion-skip-backward"></span>
-                    </button>
-                    <button className={this.props.isPlaying ? 'btn btn-success' : 'btn btn-warning'} id="play-pause" onClick={this.props.handleSongClick}>
-                        <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
-                    </button>
-                    <button id="next" onClick={this.props.handleNextClick}>
-                        <span className="ion-skip-forward"></span>
-                    </button>      
-                </section>
-                <section id="time-control">
+               
+            <section id="time-control">
                     <div className="current-time">{this.props.formatTime(this.props.currentTime)}</div>
                     <input 
                         type="range" 
@@ -28,6 +20,19 @@ class PlayerBar extends Component {
                     />
                     <div className="total-time">{this.props.formatTime(this.props.duration)}</div>
                 </section>
+
+                <section id="buttons">
+                    <button className="btn btn-dark" id="previous" onClick={this.props.handlePrevClick}>
+                        <span className="ion-skip-backward"></span>
+                    </button>
+                    <button className={this.props.isPlaying ? 'btn btn-danger' : 'btn btn-success'} id="play-pause" onClick={this.props.handleSongClick}>
+                        <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
+                    </button>
+                    <button className="btn btn-dark" id="next" onClick={this.props.handleNextClick}>
+                        <span className="ion-skip-forward"></span>
+                    </button>      
+                </section>
+                
                 <section id="volume-control">
                     <div className="icon ion-volume-low"></div>
                     <input 
